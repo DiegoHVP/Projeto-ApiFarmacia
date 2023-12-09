e40a2ad4065528da082068386047931861575c5d
DiegoHVP <dhvp10@gmail.com>
2023-12-09 00:14:15 +0000

    fix atributos

--- endpoints/medicamento.py
+++ endpoints/medicamento.py
@@ -15,12 +15,14 @@ cursor = conn.cursor()
 async def create_medicamento(m: Medicamento):
     try:
         query = """INSERT INTO Medicamento 
-                (vencimento, preco, quantidade, alergias, faixa_etaria, mg_ml, unidade, nome, farmacia_id) 
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+                (vencimento, preco, quantidade, alergias, faixa_etaria, mg_ml, unidade, nome, farmacia_id, similares, genericos, reabastecer, compra_cliente) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
         values = ( 
             m.vencimento, m.preco, m.quantidade,
             m.alergias, m.faixa_etaria, m.mg_ml,
-            m.unidade, m.nome, m.farmacia_id
+            m.unidade, m.nome, m.farmacia_id,
+            m.similares, m.genericos,
+            m.reabastecer, m.compra_cliente
         )
         
         cursor.execute(query, values)
@@ -29,7 +31,7 @@ async def create_medicamento(m: Medicamento):
     except Exception as e:
         return {"error": str(e)}
 
-# LER Medicamento POR ID
+# GET Medicamento por ID
 @router.get("/medicamento/{id}")
 async def get_medicamento(id: int):
     try:
@@ -46,78 +48,85 @@ async def get_medicamento(id: int):
             "vencimento": dadosOne[1],
             "preco": dadosOne[2],
             "quantidade": dadosOne[3],
-            "alergias": dadosOne[5],
+            "alergias": dadosOne[4],
             "faixa_etaria": dadosOne[5],
             "mg_ml": dadosOne[6],
             "unidade": dadosOne[7],
-            "farmacia_id": dadosOne[9]
+            "farmacia_id": dadosOne[9],
+            "similares": dadosOne[10],
+            "genericos": dadosOne[11],
+            "reabastecer": dadosOne[12],
+            "compra_cliente": dadosOne[13]
         }
         return {"Medicamento": medicamentoDados}
     except Exception as e:
         return {"error": str(e)}
 
-# LER TODOS OS Medicamento
+# GET Todos os Medicamentos
 @router.get("/medicamento/")
-async def getAll_medicamento():
+async def get_all_medicamento():
     try:
         query = "SELECT * FROM Medicamento"
         cursor.execute(query)
         dadosAll = cursor.fetchall()
-        
         
         medicamentosDados = []
         for dadosOne in dadosAll:
             tempDados =  {
                 "id": dadosOne[0],
                 "nome": dadosOne[8],
-                "vencimento": dadosOne[1],
                 "preco": dadosOne[2],
-                "quantidade": dadosOne[3],
-                "alergias": dadosOne[5],
-                "faixa_etaria": dadosOne[5],
                 "mg_ml": dadosOne[6],
+                "vencimento": dadosOne[1],
+                "quantidade": dadosOne[3],
+                "alergias": dadosOne[4],
+                "faixa_etaria": dadosOne[5],
                 "unidade": dadosOne[7],
-                "farmacia_id": dadosOne[9]
+                "farmacia_id": dadosOne[9],
+                "similares": dadosOne[10],
+                "genericos": dadosOne[11],
+                "reabastecer": dadosOne[12],
+                "compra_cliente": dadosOne[13]
             }
             medicamentosDados.append(tempDados)
 
         if len(medicamentosDados)!=0:
             return {"Medicamentos" : medicamentosDados}
         else:
-            return {"message":"não há medicamentos cadastrados"}
+            return {"message":"Não há medicamentos cadastrados"}
         
     except Exception as e:
         return {"error": str(e)}
 
-# ATUALIZAR Medicamento POR ID
+# UPDATE Medicamento por ID
 @router.put("/medicamento/{id}")
-async def update_medicamento(id: int, m: Medicamento):
+async def update_medicamento(id: int, medicamento: Medicamento):
     try:
-        verificar= "SELECT id FROM Medicamento WHERE id = ?"
+        verificar = "SELECT id FROM Medicamento WHERE id = ?"
         cursor.execute(verificar, (id,))
         medicamentoBool = cursor.fetchone()
 
         if not medicamentoBool:
             return {"error": f"Medicamento com ID {id} não encontrado"}
 
-
         query = """UPDATE Medicamento SET 
                 vencimento=?, preco=?, quantidade=?, alergias=?, faixa_etaria=?, 
-                mg_ml=?, unidade=?, nome=?, farmacia_id=?
+                mg_ml=?, unidade=?, nome=?, farmacia_id=?, similares=?, genericos=?, reabastecer=?, compra_cliente=?
                 WHERE id=?"""
         values = (
-            m.vencimento, m.preco, m.quantidade,
-            m.alergias, m.faixa_etaria, m.mg_ml,
-            m.unidade, m.nome, m.farmacia_id, id
+            medicamento.vencimento, medicamento.preco, medicamento.quantidade,
+            medicamento.alergias, medicamento.faixa_etaria, medicamento.mg_ml,
+            medicamento.unidade, medicamento.nome, medicamento.farmacia_id,
+            medicamento.similares, medicamento.genericos,
+            medicamento.reabastecer, medicamento.compra_cliente, id
         )
         cursor.execute(query, values)
         conn.commit()
-        return {"message": f"Medicamento {m.nome} foi atualizado"}
+        return {"message": f"Medicamento {medicamento.nome} foi atualizado"}
     except Exception as e:
         return {"error": str(e)}
 
-
-# DELETAR Medicamento POR ID
+# DELETE Medicamento por ID
 @router.delete("/medicamento/{id}")
 async def delete_medicamento(id: int):
     try:
@@ -128,7 +137,7 @@ async def delete_medicamento(id: int):
         if not dadosOne:
             return {"message": "Medicamento não encontrado"}
 
-        nomeMedicamento = dadosOne[8] #PEGA NOME
+        nomeMedicamento = dadosOne[8]  # Pega o nome do Medicamento
 
         query_delete = "DELETE FROM Medicamento WHERE id = ?"
         cursor.execute(query_delete, (id,))
